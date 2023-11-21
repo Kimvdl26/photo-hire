3d700cac99d28e7b92fa73aebda4af65c9d554c0
Ankit Kumar <ankit@gypsyphi.com>
2023-11-21 18:04:10 +0800

    Devise page minor HTML fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,5 @@ class User < ApplicationRecord
   has_many :artists
   has_many :bookings
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 end
