bd00b70e405340fd59f80293c682a6f5e44aab72
Kimberly Agustin van der Leeuw <kimberlyavdl@gmail.com>
2023-11-21 14:40:54 +0800

    Seed file (users, bookings)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,58 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.new(
+  email: "bifiyo7473@ikanid.com",
+  password: "123456",
+  password_confirmation: "123456",
+  first_name: "Charles",
+  last_name: "Moreno",
+)
+
+User.new(
+  email: "bifiyo7476@ikanid.com",
+  password: "78910",
+  password_confirmation: "78910",
+  first_name: "Sarah",
+  last_name: "Wilk",
+)
+
+User.new(
+  email: "bifiyo7480@ikanid.com",
+  password: "111213",
+  password_confirmation: "111213",
+  first_name: "Kaia",
+  last_name: "Banderas",
+)
+
+Booking.new(
+  date: "21/12/23",
+  status: "Accepted"
+)
+
+Booking.new(
+  date: "12/06/23",
+  status: "Declined"
+)
+
+Booking.new(
+  date: "21/04/23",
+  status: "Accepted"
+)
+
+# User.new(
+#   email: user["email"],
+#   password: user["password"],
+#   password_confirmation: user["password_confirmation"],
+#   first_name: user["first_name"],
+#   last_name: user["last_name"],
+#   role: user["role"],
+# )
+
+# Booking.new(
+#   user_id: booking["user_id"],
+#   artist_id: booking["artist_id"],
+#   date: booking["date"],
+#   status: booking["status"],
+# )
