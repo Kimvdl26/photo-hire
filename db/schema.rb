f4521e13ac35b1b851d8637bca16995f1eae5e58
Kimberly Agustin van der Leeuw <kimberlyavdl@gmail.com>
2023-11-22 11:53:19 +0800

    Added description and title to Artwork

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_034920) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_035224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_034920) do
     t.string "artwork"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
+    t.string "title"
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
 
