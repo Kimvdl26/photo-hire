4d3f5b02dea5ce78f6690fdfefb28b81642d23da
Michele Noten <notenm@gmail.com>
2023-11-22 17:01:26 +0800

    sign up addition, and artist_details page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :is_artist])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :is_artist])
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource.is_artist == true
+       new_artist_path
+    else
+      root_path
+    end
+  end
 end
