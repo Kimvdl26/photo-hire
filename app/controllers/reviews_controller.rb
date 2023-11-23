ee30a208c570e27e81fa4d63faf854be357d0ce7
Kimberly Agustin van der Leeuw <kimberlyavdl@gmail.com>
2023-11-23 14:32:22 +0800

    Reviewws back end done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,24 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    if @review.save
+      redirect_to artist_path(@booking.artist)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,9 @@ class Review < ApplicationRecord
   belongs_to :booking
 
   validates :booking_id, presence: true
+  validates :rating, presence: true, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }
+  validates :content, presence: true, :length => {
+    :minimum   => 10,
+    :maximum   => 100,
+  }
 end
